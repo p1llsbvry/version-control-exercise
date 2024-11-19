3434485ac5e2c1020e2c083d580e76fd1a3c05b7
p1llsbvry <raelynkang3132@gmail.com>
2024-11-18 22:00:21 -0500

    commit

--- app/stocks.py
+++ app/stocks.py
@@ -13,6 +13,8 @@ load_dotenv() # looks in the ".env" file for env vars
 
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
 
+def format_usd(my_price):
+    return f"${float(my_price):,.2f}"
 
 def fetch_stocks_csv(symbol):
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&outputsize=full&datatype=csv"
