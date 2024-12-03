c032a32a820fd20dd9faafaa681541efed921fe4
p1llsbvry <raelynkang3132@gmail.com>
2024-12-03 17:06:35 -0500

    commit deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,13 @@
 # this "app/__init__.py" file
 # helps us import from one file to another
+from flask import Flask
+from web_app.routes.home_routes import home_routes
+#from web_app.routes.stocks_routes import stocks_routes
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(home_routes)
+    #app.register_blueprint(stocks_routes)
+    return app
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
